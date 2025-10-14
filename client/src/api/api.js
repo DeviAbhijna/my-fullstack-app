32e154e6e62252572b122375a4115196e5f95a1f
Deviabhijna <deviabhijna@gmail.com>
2025-10-14 22:41:51 +0530

    Fix Contact model import

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,3 +1,7 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "/api" });
+
+const api = axios.create({
+  baseURL: "https://my-fullstack-app-2m29.onrender.com/api" // LIVE backend
+});
+
 export default api;
